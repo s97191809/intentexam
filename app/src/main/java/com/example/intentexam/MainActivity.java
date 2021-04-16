45f55e0e022c9982e515ee64337819a1c99b888b
DESKTOP-P7ESL1G\drakt <opt1997@gmail.com>
2021-04-17 00:23:04 +0900

    asd

--- app/src/main/java/com/example/intentexam/MainActivity.java
+++ app/src/main/java/com/example/intentexam/MainActivity.java
@@ -48,53 +48,35 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            //바텀 네비게이션 버튼에 눌렀을때 화면 바뀌는 기능 추가
-            //item.getItemId() 아이템의 아이디를 바로 가져옴
-            switch (item.getItemId()) {
-                case R.id.tab1:
-                    Toast.makeText(MainActivity.this, "첫 번째 탭", Toast.LENGTH_SHORT).show();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
-                    return true; //return true 밑으로는 작동이 되지 않는다.
+                //바텀 네비게이션 버튼에 눌렀을때 화면 바뀌는 기능 추가
+                //item.getItemId() 아이템의 아이디를 바로 가져옴
+                switch (item.getItemId()) {
+                    case R.id.tab1:
+                        Toast.makeText(MainActivity.this, "첫 번째 탭", Toast.LENGTH_SHORT).show();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+                        return true; //return true 밑으로는 작동이 되지 않는다.
 
-                case R.id.tab2:
-                    Toast.makeText(MainActivity.this, "두 번째 탭", Toast.LENGTH_SHORT).show();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
-                    return true;
+                    case R.id.tab2:
+                        Toast.makeText(MainActivity.this, "두 번째 탭", Toast.LENGTH_SHORT).show();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+                        return true;
 
-                case R.id.tab3:
-                    Toast.makeText(MainActivity.this, "세 번째 탭", Toast.LENGTH_SHORT).show();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment3).commit();
-                    return true;
-                case R.id.tab4:
-                    Toast.makeText(MainActivity.this, "네 번째 탭", Toast.LENGTH_SHORT).show();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment4).commit();
-                    return true;
+                    case R.id.tab3:
+                        Toast.makeText(MainActivity.this, "세 번째 탭", Toast.LENGTH_SHORT).show();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment3).commit();
+                        return true;
+                    case R.id.tab4:
+                        Toast.makeText(MainActivity.this, "네 번째 탭", Toast.LENGTH_SHORT).show();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment4).commit();
+                        return true;
+                }
+                return false;
             }
-            return false;
-        }
-        //------------------------------------네비 끝
-
-        TMapMarkerItem markerItem1 = new TMapMarkerItem();
-
-        //TMapPoint tMapPoint1 = new TMapPoint(37.570841, 126.985302); // SKT타워
+            //------------------------------------네비 끝
 
 // 마커 아이콘
 
-        final ArrayList alTMapPoint = new ArrayList();
-        alTMapPoint.add(new TMapPoint(37.576016, 126.976867));//광화문
-        alTMapPoint.add(new TMapPoint(37.570432, 126.992169));//종로3가
-        alTMapPoint.add(new TMapPoint(37.570194, 126.983045));
-        final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground);
-
-        for (int i = 0; i < alTMapPoint.size(); i++) {
-            // 마커 아이콘 지정
-            markerItem1.setIcon(bitmap);
-            // 마커의 좌표 지정
-            markerItem1.setTMapPoint((TMapPoint) alTMapPoint.get(i));
-            //지도에 마커 추가
-            tMapView.addMarkerItem("markerItem", markerItem1);
-
-        }
-
     }
+
+}
 }
