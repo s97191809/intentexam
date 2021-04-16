dc2831201fd2767a952ecfcade2a360a8e112d54
DESKTOP-P7ESL1G\drakt <opt1997@gmail.com>
2021-04-17 01:31:54 +0900

    오류 없앰

--- app/src/main/java/com/example/intentexam/loginActivity.java
+++ app/src/main/java/com/example/intentexam/loginActivity.java
@@ -11,6 +11,9 @@ public class loginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         findViewById(R.id.login_button);
+        findViewById(R.id.login_id);
+        findViewById(R.id.login_password);
+        // id pw 받아서 디비에 있는지 보고 없으면 x 있으면 메인으로 가게
     }
 
 }// MainActivity Class..
