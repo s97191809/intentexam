1f6da90127124744d7a7fc18f2501b06c0db7aab
idd_0417 <idd_0417@kakao.com>
2021-04-26 18:17:01 +0900

    캘린더 테스트

--- app/src/main/java/com/example/intentexam/FragmentPage1.java
+++ app/src/main/java/com/example/intentexam/FragmentPage1.java
@@ -1,52 +1,9 @@
 package com.example.intentexam;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import com.skt.Tmap.TMapData;
-import com.skt.Tmap.TMapGpsManager;
-import com.skt.Tmap.TMapMarkerItem;
-import com.skt.Tmap.TMapPOIItem;
-import com.skt.Tmap.TMapPoint;
-import com.skt.Tmap.TMapView;
 
-import java.util.ArrayList;
-
-public class FragmentPage1 extends Fragment{
-    //객체 선언
-    Button button2;
-    TextView textView1;
-
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-
-    }
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        setHasOptionsMenu(true);
-        Toast.makeText(this.getContext(), "This is SearchFragment", Toast.LENGTH_SHORT).show();
-        return inflater.inflate(R.layout.fragment_page_1, container, false);
-    }
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
+public class FragmentPage1 extends Fragment {
 }
+
+
+
